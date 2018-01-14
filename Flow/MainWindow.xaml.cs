aedc118028d4150c2f909c3bfb09751ee2d4598c
Crauzer <filip.quitko@gmail.com>
2018-01-14 22:54:27 +0100

    1.0.0.1

--- Flow/MainWindow.xaml.cs
+++ Flow/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Flow
 {
@@ -22,9 +10,13 @@ namespace Flow
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static readonly byte[] WAD_PATCH = { 0xB8, 0x01, 0x00, 0x00, 0x00 };
-        private static readonly byte[] WAD_PATTERN = { 0x83, 0xE6, 0xE0, 0x56, 0x53, 0x57, 0xE8 };
-        private static readonly byte WAD_REPLACE = 0xE8;
+        private static readonly byte[] WAD_ECDSA_PATCH = { 0xB8, 0x01, 0x00, 0x00, 0x00 };
+        private static readonly List<byte[]> WAD_ECDSA_PATTERNS = new List<byte[]>()
+        {
+            new byte[] { 0x83, 0xE6, 0xE0, 0x56, 0x53, 0x57, 0xE8 },
+            new byte[] { 0x93, 0x3C, 0x26, 0x00, 0x56, 0x53, 0x57, 0xE8 }
+        };
+        private static readonly byte WAD_ECDSA_REPLACE = 0xE8;
 
         public MainWindow()
         {
@@ -49,13 +41,26 @@ namespace Flow
         {
             using (FileStream fs = new FileStream(this.textboxLoLPath.Text, FileMode.Open))
             {
-                FindPattern(fs, WAD_PATTERN);
-                long offset = FindByte(fs, WAD_REPLACE);
-
-                if (offset != 0)
+                long ecdsaOffset = 0;
+                for (int i = 0; i < WAD_ECDSA_PATTERNS.Count; i++)
                 {
-                    fs.Seek(offset, SeekOrigin.Begin);
-                    fs.Write(WAD_PATCH, 0, WAD_PATCH.Length);
+                    FindPattern(fs, WAD_ECDSA_PATTERNS[i]);
+                    ecdsaOffset = FindByte(fs, WAD_ECDSA_REPLACE);
+
+                    if (ecdsaOffset != 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        fs.Seek(0, SeekOrigin.Begin);
+                    }
+                }
+
+                if (ecdsaOffset != 0)
+                {
+                    fs.Seek(ecdsaOffset, SeekOrigin.Begin);
+                    fs.Write(WAD_ECDSA_PATCH, 0, WAD_ECDSA_PATCH.Length);
 
                     MessageBox.Show("Your League of Legends executable was succesfully patched!", "Flow Succes", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
